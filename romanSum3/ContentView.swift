623f52c6881c09819ac3bd2e12eea5f6cc3464f6
Luimi55 <63078542+Luimi55@users.noreply.github.com>
2021-06-06 16:05:12 -0400

    Initial Commit

---
+++ romanSum3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  romanSum3
+//
+//  Created by Luis Miguel Rosa Reyes on 6/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ romanSum3/romanSum3App.swift
@@ -0,0 +1,17 @@
+//
+//  romanSum3App.swift
+//  romanSum3
+//
+//  Created by Luis Miguel Rosa Reyes on 6/6/21.
+//
+
+import SwiftUI
+
+@main
+struct romanSum3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
